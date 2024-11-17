207a84401e0b4027daf342768c4bf6b4e7d93ea8
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-11-17 18:20:45 +0300

    project init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { RecipeCard } from '@/components';
+
+function App() {
+  return (
+    <main className='min-h-screen sm:grid place-items-center bg-stone-1'>
+      <RecipeCard />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as RecipeCard } from './RecipeCard';
+export { default as Preparation } from './Preparation';
+export { default as RecipeTitle } from './RecipeTitle';
+export { default as Ingredients } from './Ingredients';
+export { default as Instructions } from './Instructions';
+export { default as Nutrition } from './Nutrition';
